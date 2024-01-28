b0f1acfbe3891b6749128027256be82e0c940d7d
MuhammadAreebKazmi2 <mk07202@st.habib.edu.pk>
2024-01-28 21:41:14 +0500

    Added Navbar items

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 export const Navbar = () => {
   return (
     <div className='navbar'>
-
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>HU DUKAAN</p>
+        </div>
+        <ul ckassName="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className='nav-login-cart'>
+            <button>Login</button>
+            <img src={cart_icon} alt=''></img>
+        </div>
     </div>
   )
 }
