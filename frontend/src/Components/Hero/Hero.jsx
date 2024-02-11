60f10f5d0092871ee33746905bd44abb56792a58
MuhammadAreebKazmi2 <mk07202@st.habib.edu.pk>
2024-02-11 05:06:09 +0500

    Added popular items, and exclusive banner
    Folders added:
    Offers, Popular, Items, and integrated into Shop main page

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
             <div className="hero-latest-btn">
                 <div>Latest Collection</div>
                 <img src={arrow_icon} alt=""/>
-            </div>
+            </div>\
         </div>
         <div className="hero-right">
             <img src={hero_image} alt=""></img>
