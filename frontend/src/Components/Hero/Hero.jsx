a951d14e251752031588e0b7d9b83aab8b90a51c
MuhammadAreebKazmi2 <mk07202@st.habib.edu.pk>
2024-03-28 22:06:50 +0500

    Modified HomePage

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import hero_image from '../Assets/logo_big.png'
 
 const Hero = () => {
   return (
     <div className='Hero'>
         <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
+            <h2>HU Dukaan x HU</h2>
             <div>
                 <div className="hero-hand-icon">
-                    <p>new</p>
+                    <p>elevating</p>
                     <img src={hand_icon} alt=""/>
                 </div>
-                <p>collections</p>
-                <p>for everyone</p>
+                <p>community's</p>
+                <p>experience</p>
             </div>
             <div className="hero-latest-btn">
                 <div>Latest Collection</div>
                 <img src={arrow_icon} alt=""/>
-            </div>\
+            </div>
         </div>
         <div className="hero-right">
             <img src={hero_image} alt=""></img>
