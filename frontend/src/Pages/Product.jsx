d037370101f396ac47c2e8c94d55b0b75e953c46
MuhammadAreebKazmi2 <mk07202@st.habib.edu.pk>
2024-03-28 13:24:24 +0500

    Uncommitted the last commit

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,7 +2,7 @@
 import React, { useContext } from 'react'
 import {ShopContext} from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
-import Breadcrum from '../Components/Breadcrums/Breadcrum.jsx';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -10,12 +10,16 @@ const Product = () => {
   
   // Conditional rendering to avoid errors when things still undefined 
   const product = all_product ? all_product.find((e) => e.id === Number(productId)) : null;
+<<<<<<< HEAD
   //const product = all_product.find((e) => e.id === productId) ;
 =======
 import React from 'react'
 
 const Product = () => {
 >>>>>>> parent of 4cc81d3 (Linking products to their pages)
+=======
+   
+>>>>>>> parent of adbbb3a (Exception handling)
   return (
     <div>Product</div>
   )
