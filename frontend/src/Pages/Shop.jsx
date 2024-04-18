bc51b8ab3de91b7efd79a55c980f8487206d6ba6
AyeshaSaeed328 <as07519@st.habib.edu.pk>
2024-04-19 04:23:29 +0500

    frontend fetch products from database

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/New Collections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
+
 const Shop = () => {
+  
+  
+
   return (
     <div>
         <Hero/>
         {/* <Popular/> */}
         <Offers/>
-        <NewCollections/>
+        {/* <NewCollections/> */}
         <NewsLetter/>
     </div>
   )
